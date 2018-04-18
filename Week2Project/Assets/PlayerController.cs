d0d954f4494ece06c45816d1150ccc58fca52475
Tranzorro <shiekadam@gmail.com>
2018-04-17 21:08:07 -0400

    jump
    its a thing

---
+++ Week2Project/Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Rigidbody2D Rigid;
+    public float JumpForce;
+
+    // Use this for initialization
+    void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Rigid.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+        }
+
+    }
+}
